a67509745799403c90335486b62ca9e19fa0104e
carlosgomes1 <carlos_gomes.s@hotmail.com>
2020-10-28 15:49:09 -0300

    letter machine ok

--- src/js/register.js
+++ src/js/register.js
@@ -2,7 +2,6 @@ var login = true;
 var formLogin = document.querySelector('#login');
 var formRegister = document.querySelector('#cadastro');
 
-
 document.querySelector('#create').addEventListener('click', () => {
     handleChange();
 })
@@ -10,6 +9,22 @@ document.querySelector('#create').addEventListener('click', () => {
 document.querySelector('#own').addEventListener('click', () => {
     handleChange();
 })
+
+document.querySelector('#button-login').addEventListener('click', () => {
+    handleLogin();
+})
+
+const handleLogin = () => {
+    let inputLoginEmail = document.getElementById('login-email').value;
+    let inputLoginPassword = document.getElementById('login-password').value;
+
+    if (inputLoginEmail === 'admin@admin.com' && inputLoginPassword === 'admin') {
+        window.location.href = './index.html';
+    } else {
+        alert('E-mail ou senha incorretos, tente novamente.')
+    }
+
+}
 
 const handleChange = () => {
     login = !login;
